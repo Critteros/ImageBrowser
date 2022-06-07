221b9c962cb10757e4007bb3fd51d61a4888fa35
Critteros <szymonkoziol3@gmail.com>
2022-06-07 02:00:30 +0200

    Added zlib and exiv2 dependencies, removed .idea from git

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -3,7 +3,8 @@
 
 #include "FileExplorer.hpp"
 
-
+#include <iostream>
+#include <exiv2/exiv2.hpp>
 
 MainWindow::MainWindow() : QMainWindow() {
 
@@ -21,6 +22,24 @@ MainWindow::MainWindow() : QMainWindow() {
 
     m_splitter->setMinimumSize(MAIN_WIDGET_MIN_SIZE);
 
+    auto image = Exiv2::ImageFactory::open("test.jpg");
+    image->readMetadata();
+    Exiv2::ExifData &exifData = image->exifData();
+    Exiv2::ExifData::const_iterator end = exifData.end();
+    for (Exiv2::ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
+        const char *tn = i->typeName();
+        std::cout << std::setw(44) << std::setfill(' ') << std::left
+                  << i->key() << " "
+                  << "0x" << std::setw(4) << std::setfill('0') << std::right
+                  << std::hex << i->tag() << " "
+                  << std::setw(9) << std::setfill(' ') << std::left
+                  << (tn ? tn : "Unknown") << " "
+                  << std::dec << std::setw(3)
+                  << std::setfill(' ') << std::right
+                  << i->count() << "  "
+                  << std::dec << i->value()
+                  << "\n";
+    }
 
     setCentralWidget(m_splitter);
 }
